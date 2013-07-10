2f6efcbd51cb6de79c0a7ebdd67139ebcee2c344
Fortune.Zhang <zhangfortune527@gmail.com>
2013-07-10 15:30:52 +0800

    re init jasmine

---
+++ spec_helper/hello_world_spec_helper.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: fortunezhang
+ * Date: 13-7-10
+ * Time: 下午3:30
+ * To change this template use File | Settings | File Templates.
+ */
+
+function hello_world_spec_helper()
+{
+
+}
